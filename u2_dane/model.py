f52942dde4c5a320e6087688c37c369a56dba863
Dane M <mitrevdane@gmail.com>
2020-07-12 01:11:39 +0200

    fix: fixed layer output ch in RSUF.

--- u2_dane/model.py
+++ u2_dane/model.py
@@ -19,7 +19,7 @@ class ConvBNReluBlock(nn.Module):
 
 
 class GenericRSUBlock(nn.Module):
-    def __init__(self, input_ch, mid_ch, output_ch, L):
+    def __init__(self, input_ch=3, mid_ch=12, output_ch=3, L=None):
         super(GenericRSUBlock, self).__init__()
 
         self._L = L
@@ -39,12 +39,18 @@ class GenericRSUBlock(nn.Module):
 
         # decoder
         decoder = []
-        for i in range(L - 1):  # decoder has -1 channel
+        for i in range(L - 2):  # decoder has -1 channel, -1 for the last channel out
             decoder.append(ConvBNReluBlock(input_ch=mid_ch*2, output_ch=mid_ch, dirate=1))  # TODO: verify mid_ch * 2
+
+        decoder.append(ConvBNReluBlock(input_ch=mid_ch*2, output_ch=output_ch, dirate=1))  # TODO: verify mid_ch * 2
+
 
         # print(nn.Sequential(*encoder, self.last_layer_enc, *decoder))
         self.encoder = nn.Sequential(*encoder)
         self.decoder = nn.Sequential(*decoder)
+
+    def __repr__(self):
+        return "Block RSU: " + str(self._L)
 
     def forward(self, x):
         outputs = []
@@ -62,10 +68,13 @@ class GenericRSUBlock(nn.Module):
         # decoder
         upward = self.last_layer_enc.forward(downward)
         for layer in self.decoder[:-1]:
-            upward = layer(torch.cat((outputs.pop(), upward), 1))
+            upward = layer(torch.cat((upward, outputs.pop()), 1))
             upward = _upsample_like(upward, outputs[-1])
+            # print("upsampling: ", layer)
 
-        return hx_in + self.decoder[-1](torch.cat((outputs.pop(), upward), 1))
+        # print(self)
+        # print(outputs[-1].size(), upward.size(), hx_in.size())
+        return hx_in + self.decoder[-1](torch.cat((upward, outputs.pop()), 1))
 
 
 class GenericRSUFBlock(nn.Module):
@@ -80,10 +89,14 @@ class GenericRSUFBlock(nn.Module):
             # print("enc: ", 2 ** i)
             encoder.append(ConvBNReluBlock(input_ch=out_ch if i == 0 else mid_ch, output_ch=mid_ch, dirate=2 ** i))
 
-        for i in range(L-1):
-            # print(i)
+        for i in range(L-2):
+            print(i)
             # print("dec: ", 2 ** (L-2-i))
             decoder.append(ConvBNReluBlock(input_ch=mid_ch * 2, output_ch=mid_ch, dirate=2 ** (L-2-i)))
+
+        print(i)
+        decoder.append(ConvBNReluBlock(input_ch=mid_ch * 2, output_ch=out_ch, dirate=2 ** (L-2-i)))
+
 
         self.encoder = nn.Sequential(*encoder)
         self.decoder = nn.Sequential(*decoder)
@@ -130,10 +143,74 @@ class U2SquaredNet(nn.Module):
         self.stage5d = GenericRSUFBlock(128, 16, 64, L=4)
         self.stage4d = GenericRSUBlock(128, 16, 64, L=4)
         self.stage3d = GenericRSUBlock(128, 16, 64, L=5)
-        self.stage4d = GenericRSUBlock(128, 16, 64, L=6)
+        self.stage2d = GenericRSUBlock(128, 16, 64, L=6)
         self.stage1d = GenericRSUBlock(128, 16, 64, L=7)
 
+        self.side1 = nn.Conv2d(64, out_ch, 3, padding=1)
+        self.side2 = nn.Conv2d(64, out_ch, 3, padding=1)
+        self.side3 = nn.Conv2d(64, out_ch, 3, padding=1)
+        self.side4 = nn.Conv2d(64, out_ch, 3, padding=1)
+        self.side5 = nn.Conv2d(64, out_ch, 3, padding=1)
+        self.side6 = nn.Conv2d(64, out_ch, 3, padding=1)
+
         self.out_conv = nn.Conv2d(6, out_ch, 1)
+
+    def forward(self, x):
+        hx = x
+
+        hx1 = self.stage1.forward(hx)
+        hx = self.pool12(hx1)
+
+        hx2 = self.stage2.forward(hx)
+        hx = self.pool23(hx2)
+
+        hx3 = self.stage3.forward(hx)
+        hx = self.pool34(hx3)
+
+        hx4 = self.stage4.forward(hx)
+        hx = self.pool45(hx4)
+
+        hx5 = self.stage5.forward(hx)
+        hx = self.pool56(hx5)
+
+        hx6 = self.stage6.forward(hx)
+        hx6_up = _upsample_like(hx6, hx5)
+
+        # decoder
+        hx5d = self.stage5d.forward(torch.cat((hx6_up, hx5), 1))
+        hx5d_up = _upsample_like(hx5d, hx4)
+
+        hx4d = self.stage4d.forward(torch.cat((hx5d_up, hx4), 1))
+        hx4d_up = _upsample_like(hx4d, hx3)
+
+        hx3d = self.stage3d.forward(torch.cat((hx4d_up, hx3), 1))
+        hx3d_up = _upsample_like(hx3d, hx2)
+
+        hx2d = self.stage2d.forward(torch.cat((hx3d_up, hx2), 1))
+        hx2d_up = _upsample_like(hx2d, hx1)
+
+        hx1d = self.stage1d.forward(torch.cat((hx2d_up, hx1), 1))
+
+        # side output TODO: What are side outputs used for?
+        d1 = self.side1(hx1d)
+
+        d2 = self.side2(hx2d)
+        d2 = _upsample_like(d2, d1)
+
+        d3 = self.side3(hx3d)
+        d3 = _upsample_like(d3, d1)
+
+        d4 = self.side4(hx4d)
+        d4 = _upsample_like(d4, d1)
+
+        d5 = self.side5(hx5d)
+        d5 = _upsample_like(d5, d1)
+
+        d6 = self.side6(hx6)
+        d6 = _upsample_like(d6, d1)
+
+        d0 = self.out_conv(torch.cat((d1, d2, d3, d4, d5, d6), 1))
+        return F.sigmoid(d0), F.sigmoid(d1), F.sigmoid(d2), F.sigmoid(d3), F.sigmoid(d4), F.sigmoid(d5), F.sigmoid(d6)
 
 
 if __name__ == '__main__':
@@ -146,4 +223,5 @@ if __name__ == '__main__':
     # c1f.forward(torch.Tensor(torch.rand((1, 3, 256, 256))))
 
     # U-squared Generic
+
 
